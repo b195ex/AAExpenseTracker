2dfbf33295f8c4a91ac194a75e823386a7e22b20
Eric Viera <b195ex@hotmail.com>
2020-01-30 13:23:43 -0600

    Weird query to get remaining income

---
+++ AAExpenseTracker/AddAlert.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="AddAlert.aspx.cs" Inherits="AAExpenseTracker.AddAlert" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Panel ID="Panel1" runat="server">
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    </asp:Panel>
+</asp:Content>
---
+++ SQLQuery1.sql
@@ -0,0 +1,49 @@
+﻿select sum(TotalIncome)-sum(totalexpense) as Remaining
+from(
+	select SUM(amount) TotalIncome, 0 TotalExpense
+	from(
+		select isnull(sum(amount),0) amount 
+		from FixIncoms
+		where FixIncoms.User_UserID='b195ex@hotmail.com'
+		union
+		select isnull(sum(amount),0) amount 
+		from Incomes
+		where Incomes.User_UserID='b195ex@hotmail.com'
+	) as incomes
+	union
+	select 0 TotalIncome, SUM(amount) TotalExpense
+	from(
+		select isnull(sum(amount),0) amount 
+		from FixExpens
+		where User_UserID='b195ex@hotmail.com'
+		union
+		select isnull(sum(amount),0) amount 
+		from Expenses
+		where User_UserID='b195ex@hotmail.com'
+	) as expenses
+) as disposable
+
+select sum(TotalIncome)-sum(totalexpense) as Remaining
+from (
+	select SUM(amount) TotalIncome, 0 TotalExpense
+	from(
+		select isnull(sum(amount),0) amount 
+		from FixIncoms
+		where FixIncoms.User_UserID='Archangel'
+		union
+		select isnull(sum(amount),0) amount 
+		from Incomes
+		where Incomes.User_UserID='Archangel'
+	) as incomes
+	union
+	select 0 TotalIncome, SUM(amount) TotalExpense
+	from (
+		select isnull(sum(amount),0) amount 
+		from FixExpens
+		where User_UserID='Archangel'
+		union
+		select isnull(sum(amount),0) amount 
+		from Expenses
+		where User_UserID='Archangel'
+	) as expenses
+) as disposable
